3f26d1c678748263e8cd7e6f068353212966eb46
mrunelov <martin.runelov@gmail.com>
2013-12-16 17:29:06 +0100

    Added some sample code

--- Weka.java
+++ Weka.java
@@ -14,5 +14,16 @@ public class Weka {
         // For example, the XRFF format saves the class attribute information as well
         if (data.classIndex() == -1)
             data.setClassIndex(0);
+
+        // TODO: Fix code that works. STWV code copied from example.
+        StringToWordVector filter = new StringToWordVector();
+        filter.setInputFormat(data);
+        Instances dataFiltered = Filter.useFilter(data, filter);
+        System.out.println("\n\nFiltered data:\n\n" + dataFiltered);
+
+        // train J48 and output model
+        J48 classifier = new J48();
+        classifier.buildClassifier(dataFiltered);
+        System.out.println("\n\nClassifier model:\n\n" + classifier);
     }
 }
