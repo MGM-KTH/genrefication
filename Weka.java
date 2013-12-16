ca70497b2258be45337530e7aa9ab1ccfdf6cfa0
glindste <gustaflindstedt@gmail.com>
2013-12-16 16:24:29 +0100

    wrong filepath

--- Weka.java
+++ Weka.java
@@ -8,7 +8,7 @@ import weka.filters.*;
 
 public class Weka {
     public static void main(String[] args) throws Exception{
-        DataSource source = new DataSource("data/horror_other.arff");
+        DataSource source = new DataSource("horror_other.arff");
         Instances data = source.getDataSet();
         // setting class attribute if the data format does not provide this information
         // For example, the XRFF format saves the class attribute information as well
